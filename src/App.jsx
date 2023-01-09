421a9b66e5ed2d28c31b453e00db5a5d7733aea7
Rezaapps <122305716+Rezaapps@users.noreply.github.com>
2023-01-09 21:22:30 +0000

    added tests

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Dashboard from './pages/Dashboard'
+import Editor from './pages/Editor'
+import Register from './pages/Register'
+
+function App() {
+  return (
+    <>
+		{/* <Router basename={process.env.REACT_APP_BASENAME} > */}
+		<Router >
+			<div className='container'>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Dashboard />} />
+				<Route path='/editor/:id' element={<Editor />} />
+				<Route path='/register' element={<Register />} />
+			</Routes>
+			</div>
+      </Router>
+    </>
+  )
+}
+
+export default App
